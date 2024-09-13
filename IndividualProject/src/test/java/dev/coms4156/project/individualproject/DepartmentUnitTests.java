f857049c55c88e55a91cb784ffbe56d50e9dec66
ColumbianJayz <jam2509@columbia.edu>
2024-09-13 18:09:23 -0400

    Created Unit Test for RouteController

--- IndividualProject/src/test/java/dev/coms4156/project/individualproject/DepartmentUnitTests.java
+++ IndividualProject/src/test/java/dev/coms4156/project/individualproject/DepartmentUnitTests.java
@@ -1,21 +1,28 @@
 package dev.coms4156.project.individualproject;
 
+import java.util.HashMap;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import java.util.HashMap;
-
+/**
+ * Sets up the testDepartment test suite.
+ */
 @SpringBootTest
 @ContextConfiguration
 public class DepartmentUnitTests {
-
+  /**
+   * Makes testDepartment a global variable.
+   */
   public static Department testDepartment;
 
+  /**
+   * Sets Department up for testing.
+   */
   @BeforeAll
   public static void setupDepartmentForTesting() {
     String[] times = {"11:40-12:55", "4:10-5:25", "10:10-11:25", "2:40-3:55"};
@@ -29,14 +36,14 @@ public class DepartmentUnitTests {
   }
 
   @Test
-  public void getNumberOfMajorsTest(){
+  public void getNumberOfMajorsTest() {
     int expectedNumberTest = 2700;
     int actualNumber = testDepartment.getNumberOfMajors();
     assertEquals(expectedNumberTest, actualNumber);
   }
 
   @Test
-  public void getDepartmentChairTest(){
+  public void getDepartmentChairTest() {
     String expectedDepartmentChair = "Luca Carloni";
     String actualDepartmentChair = testDepartment.getDepartmentChair();
     assertEquals(actualDepartmentChair, expectedDepartmentChair);
@@ -44,7 +51,7 @@ public class DepartmentUnitTests {
 
 
   @Test
-  public void addPersonToMajorTest(){
+  public void addPersonToMajorTest() {
     int expectedNumberOfMajors  = 2700;
     testDepartment.addPersonToMajor();
     int newNumberOfMajors = testDepartment.getNumberOfMajors();
@@ -52,7 +59,7 @@ public class DepartmentUnitTests {
   }
 
   @Test
-  public void dropPersonFromMajorTest(){
+  public void dropPersonFromMajorTest() {
     int expectedNumOfMajors = 2699;
     testDepartment.dropPersonFromMajor();
     int newNumberOfMajors = testDepartment.getNumberOfMajors();
@@ -60,28 +67,32 @@ public class DepartmentUnitTests {
   }
 
   @Test
-  public void addCourseTest(){
+  public void addCourseTest() {
     String[] times = {"11:40-12:55", "4:10-5:25", "10:10-11:25", "2:40-3:55"};
-    String courseIDTest = "4018";
+    String courseIdTest = "4018";
     Course phys4018 = new Course("James W McIver", "307 PUP", times[3], 30);
-    testDepartment.addCourse(courseIDTest, phys4018);
-    assertTrue(testDepartment.getCourseSelection().containsKey(courseIDTest));
+    testDepartment.addCourse(courseIdTest, phys4018);
+    assertTrue(testDepartment.getCourseSelection().containsKey(courseIdTest));
   }
 
   @Test
-  public void createCourseTest(){
-    String courseIDTest = "4019";
+  public void createCourseTest() {
+    String courseIdTest = "4019";
     String testName = "James W McIver";
     String testCourseLocation = "307 PUP";
     String testCourseTimeSlot = "11:40-12:55";
     int testCapacity = 15;
-    testDepartment.createCourse(courseIDTest, testName, testCourseLocation, testCourseTimeSlot, testCapacity);
-    assertTrue(testDepartment.getCourseSelection().containsKey(courseIDTest));
+    testDepartment.createCourse(courseIdTest, testName, 
+        testCourseLocation, 
+        testCourseTimeSlot, testCapacity);
+    assertTrue(testDepartment.getCourseSelection().containsKey(courseIdTest));
   }
 
   @Test 
-  public void toStringTest(){
-    String expectedString = "COMS 1001: \nInstructor: Adam Cannon; Location: 417 IAB; Time: 11:40-12:55\n";
+  public void toStringTest() {
+    String expectedString =
+        "COMS 1001: \nInstructor: Adam Cannon;" 
+        + "Location: 417 IAB; Time: 11:40-12:55\n";
     String actualString = testDepartment.toString();
     assertEquals(expectedString, actualString);
   }
--- IndividualProject/src/test/java/dev/coms4156/project/individualproject/RouteControllerTests.java
+++ IndividualProject/src/test/java/dev/coms4156/project/individualproject/RouteControllerTests.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import java.util.HashMap;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,11 +22,115 @@ import org.springframework.web.bind.annotation.RestController;
 @ContextConfiguration
 
 public class RouteControllerTests {
+  /**
+   * Setting these up as global variables.
+   */
+  public static RouteController routeController;
+  public static HashMap<String, Department> departmentMapping;
   
-  public static RouteController Route;
-
   @BeforeAll
+  public static void setupDepartmentForTesting() {
+    routeController = new RouteController();
+    HashMap<String, Course> courses = new HashMap<>();
+    departmentMapping = new HashMap<>();
+    Course coms1004 = new Course("Adam Cannon", "417 IAB", "11:40-12:55", 400);
+    courses.put("1001", coms1004);
+    String keyTest = "COMS";
+    Department testDepartment = new Department("COMS", courses, "Luca Carloni", 2700);
+    departmentMapping.put(keyTest, testDepartment);
+    IndividualProjectApplication.myFileDatabase = new MyFileDatabase(0, "./data.txt");
+    IndividualProjectApplication.myFileDatabase.setMapping(departmentMapping);
+  }
 
+  @Test
+  public void retrieveDepartmentTest() {
+    ResponseEntity<?> testResponse = routeController.retrieveDepartment("COMS");
+    assertEquals(HttpStatus.OK, testResponse.getStatusCode());
+  }
 
+  @Test
+  public void retrieveCourseTest() {
+    ResponseEntity<?> testResponse = routeController.retrieveCourse("COMS", 1001);
+    assertEquals(HttpStatus.OK, testResponse.getStatusCode());
+  }
+
+  @Test
+  public void isCourseFullTest() {
+    ResponseEntity<?> testResponse = routeController.isCourseFull("COMS", 1001);
+    assertEquals(HttpStatus.OK, testResponse.getStatusCode());
+  }
+
+  @Test
+  public void getMajorCountFromDeptTest() {
+    ResponseEntity<?> testResponse = routeController.getMajorCtFromDept("COMS");
+    assertEquals(HttpStatus.OK, testResponse.getStatusCode());
+  }
+
+  @Test
+  public void identifyDeptChairTest() {
+    ResponseEntity<?> testResponse = routeController.identifyDeptChair("COMS");
+    assertEquals(HttpStatus.OK, testResponse.getStatusCode());
+  }
+
+  @Test
+  public void findCourseLocation() {
+    ResponseEntity<?> testResponse = routeController.findCourseLocation("COMS", 1001);
+    assertEquals(HttpStatus.OK, testResponse.getStatusCode());
+  }
+
+  @Test
+  public void findCourseInstructor() {
+    ResponseEntity<?> testResponse = routeController.findCourseInstructor("COMS", 1001);
+    assertEquals(HttpStatus.OK, testResponse.getStatusCode());
+  }
+
+  @Test
+  public void findCourseTimeTest() {
+    ResponseEntity<?> testResponse = routeController.findCourseTime("COMS", 1001);
+    assertEquals(HttpStatus.OK, testResponse.getStatusCode());
+
+  }
+
+  @Test
+  public void addMajorToDeptTest() {
+    ResponseEntity<?> testResponse = routeController.addMajorToDept("COMS");
+    assertEquals(HttpStatus.OK, testResponse.getStatusCode());
+  }
+  
+  @Test
+  public void removeMajorFromDeptTest(){
+    ResponseEntity<?> testResponse = routeController.removeMajorFromDept("COMS");
+    assertEquals(HttpStatus.OK, testResponse.getStatusCode());
+  }
+
+  @Test
+  public void dropStudentFromCourseTest(){
+    ResponseEntity<?> testResponse = routeController.dropStudent("COMS", 1001);
+    assertEquals(HttpStatus.OK, testResponse.getStatusCode());
+  }
+
+  @Test
+  public void setEnrolledStudentCountTest() {
+    ResponseEntity<?> testResponse = routeController.setEnrollmentCount("COMS", 1001, 2);
+    assertEquals(HttpStatus.OK, testResponse.getStatusCode());
+  }
+
+  @Test
+  public void changeCourseTimeTest() {
+    ResponseEntity<?> testResponse = routeController.changeCourseTime("COMS", 1001, "11:40-12:55");
+    assertEquals(HttpStatus.OK, testResponse.getStatusCode());
+  }
+
+  @Test
+  public void changeCourseTeacher() {
+    ResponseEntity<?> testResponse = routeController.changeCourseTeacher("COMS", 1001, "Margaret Thatcher");
+    assertEquals(HttpStatus.OK, testResponse.getStatusCode());
+  }
+
+  @Test
+  public void changeCourseLocation() {
+    ResponseEntity<?> testResponse = routeController.changeCourseLocation("COMS", 1001, "PUP 305");
+    assertEquals(HttpStatus.OK, testResponse.getStatusCode());
+  }
   
 }
